99709bac9c97b3c25dbf4ddf68de49fdcce64aaf
Felix Kim <felix.l.kim@gmail.com>
2021-05-14 18:31:39 -0400

    Percolation passed tests

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -8,37 +8,115 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-    private WeightedQuickUnionUF uf1;
-    private WeightedQuickUnionUF uf2;
+    private WeightedQuickUnionUF ufTop; //
+    private WeightedQuickUnionUF ufAll; //
+    private final int n; //
+    private int openCount; //
+
+    private boolean[][] open; //
+    private final int TOP_IDX; //
+    private final int BOTTOM_IDX; //
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
-        grid = new boolean[n][n];
+        if (n < 1) {
+            throw new IllegalArgumentException();
+        }
+
+        // The very last array entry will represent "outside of the grid"
+        ufTop = new WeightedQuickUnionUF(n*n+1);
+        // The very last two array entries will represent "outside of the grid"
+        ufAll = new WeightedQuickUnionUF(n*n+2);
+
+        open = new boolean[n][n]; // By default, false
+        openCount = 0;
+
+        TOP_IDX = n*n;
+        BOTTOM_IDX = n*n + 1;
+
+        this.n = n;
+    }
+
+    private int map_toUFIdx(int row, int col) {
+        int adjRow = row-1;
+        int adjCol = col-1;
+
+        return adjRow * n + adjCol;
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
-        return;
+        if (row < 1 || row > n || col < 1 || col > n) {
+            throw new IllegalArgumentException();
+        }
+
+        if (isOpen(row, col)) {
+            return;
+        }
+
+        int adjRow = row-1;
+        int adjCol = col-1;
+
+        // Flag as open
+        open[adjRow][adjCol] = true;
+        openCount++;
+
+        // Treat an entry in the top row differently
+        if (adjRow == 0) {
+            ufTop.union(map_toUFIdx(row,col), TOP_IDX);
+            ufAll.union(map_toUFIdx(row,col), TOP_IDX);
+        }
+
+        // Treat an entry in the bottom row differently
+        if (adjRow == n-1) {
+            ufAll.union(map_toUFIdx(row,col), BOTTOM_IDX);
+        }
+
+        // Connect with each adjacent *open* site
+        if (adjCol > 0 && isOpen(row, col-1)) { // Not on left edge
+            ufTop.union(map_toUFIdx(row,col), map_toUFIdx(row,col-1));
+            ufAll.union(map_toUFIdx(row,col), map_toUFIdx(row,col-1));
+        }
+        if (adjCol < n-1 && isOpen(row, col+1)) { // Not on right edge
+            ufTop.union(map_toUFIdx(row,col), map_toUFIdx(row,col+1));
+            ufAll.union(map_toUFIdx(row,col), map_toUFIdx(row,col+1));
+        }
+        if (adjRow > 0 && isOpen(row-1, col)) { // Not on top edge
+            ufTop.union(map_toUFIdx(row,col), map_toUFIdx(row-1,col));
+            ufAll.union(map_toUFIdx(row,col), map_toUFIdx(row-1,col));
+        }
+        if (adjRow < n-1 && isOpen(row+1, col)) { // Not on bottom edge
+            ufTop.union(map_toUFIdx(row,col), map_toUFIdx(row+1,col));
+            ufAll.union(map_toUFIdx(row,col), map_toUFIdx(row+1,col));
+        }
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        return true;
+        if (row < 1 || row > n || col < 1 || col > n) {
+            throw new IllegalArgumentException();
+        }
+
+        return open[row-1][col-1];
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        return true;
+        if (row < 1 || row > n || col < 1 || col > n) {
+            throw new IllegalArgumentException();
+        }
+
+        return ufTop.find(TOP_IDX) == ufTop.find(map_toUFIdx(row, col));
     }
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        return 0;
+        return openCount;
     }
 
     // does the system percolate?
     public boolean percolates() {
-        return true;
+        return ufAll.find(TOP_IDX) == ufAll.find(BOTTOM_IDX);
     }
 
     // test client (optional)
--- percolation/PercolationStats.java
+++ percolation/PercolationStats.java
@@ -4,35 +4,63 @@
  *  Last modified:     1/1/2019
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
+
+    private double[]results;
+    private int numGridCells;
 
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException();
+        }
 
+        results = new double[trials];
+        numGridCells = n*n;
+
+        for (int i = 0; i < trials; i++) {
+            Percolation p = new Percolation(n);
+            while (!p.percolates()) {
+                p.open(StdRandom.uniform(n)+1, StdRandom.uniform(n)+1);
+            }
+            // Finally p percolates
+            results[i] = ((double) p.numberOfOpenSites()) / numGridCells;
+        }
     }
 
     // sample mean of percolation threshold
     public double mean() {
-
+        return StdStats.mean(results);
     }
 
     // sample standard deviation of percolation threshold
     public double stddev() {
-
+        return StdStats.stddev(results);
     }
 
     // low endpoint of 95% confidence interval
     public double confidenceLo() {
-
+        return mean() - 1.96*stddev() / Math.sqrt(results.length);
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi() {
-
+        return mean() + 1.96*stddev() / Math.sqrt(results.length);
     }
 
     // test client (see below)
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+
+        PercolationStats ps = new PercolationStats(n, trials);
+        System.out.println("mean = " + ps.mean());
+        System.out.println("stddev = " + ps.stddev());
+        System.out.println("95% confidence interval = " + "[" + ps.confidenceLo() + ", " + ps.confidenceHi() + "]");
+    }
 
 
 }
