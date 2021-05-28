b6773b83be5dd0feaafa299f9f4a7efada248956
Felix Kim <felix.l.kim@gmail.com>
2021-05-28 14:15:46 -0400

    Finished Point class

--- collinear/Point.java
+++ collinear/Point.java
@@ -2,7 +2,7 @@
  *  Compilation:  javac Point.java
  *  Execution:    java Point
  *  Dependencies: none
- *  
+ *
  *  An immutable data type for points in the plane.
  *  For use on Coursera, Algorithms Part I programming assignment.
  *
@@ -10,6 +10,7 @@
 
 import edu.princeton.cs.algs4.StdDraw;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class Point implements Comparable<Point> {
@@ -20,8 +21,8 @@ public class Point implements Comparable<Point> {
     /**
      * Initializes a new point.
      *
-     * @param  x the <em>x</em>-coordinate of the point
-     * @param  y the <em>y</em>-coordinate of the point
+     * @param x the <em>x</em>-coordinate of the point
+     * @param y the <em>y</em>-coordinate of the point
      */
     public Point(int x, int y) {
         /* DO NOT MODIFY */
@@ -56,12 +57,23 @@ public class Point implements Comparable<Point> {
      * Double.POSITIVE_INFINITY if the line segment is vertical;
      * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
      *
-     * @param  that the other point
+     * @param that the other point
      * @return the slope between this point and the specified point
      */
     public double slopeTo(Point that) {
         /* YOUR CODE HERE */
-        return 0.0;
+        if (compareTo(that) == 0) {
+            return Double.NEGATIVE_INFINITY;
+        }
+        else if (this.x == that.x) {
+            return Double.POSITIVE_INFINITY;
+        }
+        else if (this.y == that.y) {
+            return +0.0;
+        }
+        else {
+            return ((double) (that.y - this.y)) / (that.x - this.x);
+        }
     }
 
     /**
@@ -69,21 +81,42 @@ public class Point implements Comparable<Point> {
      * Formally, the invoking point (x0, y0) is less than the argument point
      * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
      *
-     * @param  that the other point
+     * @param that the other point
      * @return the value <tt>0</tt> if this point is equal to the argument
-     *         point (x0 = x1 and y0 = y1);
-     *         a negative integer if this point is less than the argument
-     *         point; and a positive integer if this point is greater than the
-     *         argument point
+     * point (x0 = x1 and y0 = y1);
+     * a negative integer if this point is less than the argument
+     * point; and a positive integer if this point is greater than the
+     * argument point
      */
     public int compareTo(Point that) {
         /* YOUR CODE HERE */
-        return 0;
+        if (this.y < that.y) {
+            return -1;
+        }
+        else if (this.y > that.y) {
+            return 1;
+        }
+        else { // Same y-coordinate
+            if (this.x < that.x) {
+                return -1;
+            }
+            else if (this.x > that.x) {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
     }
 
     private class MyComparator implements Comparator<Point> {
         public int compare(Point p1, Point p2) {
-            return 0;
+            double slope1 = slopeTo(p1);
+            double slope2 = slopeTo(p2);
+
+            if (slope1 < slope2) return -1;
+            else if (slope2 < slope1) return 1;
+            else return 0;
         }
     }
 
@@ -116,5 +149,36 @@ public class Point implements Comparable<Point> {
      */
     public static void main(String[] args) {
         /* YOUR CODE HERE */
+        System.out.println("Kello, Point class!");
+
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(1, 2);
+        Point p3 = new Point(2, 1);
+        Point p4 = new Point(2, 2);
+        Point p5 = new Point(1, 1);
+
+        assert p1.compareTo(p5) == 0;
+        assert p1.compareTo(p2) < 0;
+        assert p1.compareTo(p3) < 0;
+        assert p1.compareTo(p4) < 0;
+        assert p2.compareTo(p3) > 0;
+        assert p2.compareTo(p4) < 0;
+        assert p3.compareTo(p4) < 0;
+
+        assert p1.slopeTo(p5) == Double.NEGATIVE_INFINITY;
+        assert p1.slopeTo(p2) == Double.POSITIVE_INFINITY;
+        assert p1.slopeTo(p3) == 0.0;
+        assert p1.slopeTo(p4) == 1.0;
+        assert p2.slopeTo(p3) == -1.0;
+        assert p2.slopeTo(p4) == 0.0;
+        assert p3.slopeTo(p4) == Double.POSITIVE_INFINITY;
+
+        Comparator<Point> trialComparator = p1.slopeOrder();
+
+        Point[] points = { p1, p2, p3, p4, p5 };
+        Arrays.sort(points, trialComparator);
+        for (Point point : points) {
+            System.out.println(point);
+        }
     }
 }
