aaab1acdf3089bfbab0e07bf8df5d98d855c877f
Felix Kim <felix.l.kim@gmail.com>
2021-07-21 11:12:43 -0400

    Added separate chaining implementation for hash table

--- HashTables/src/com/company/Main.java
+++ HashTables/src/com/company/Main.java
@@ -3,7 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("hello world!");
+        // Usage of the class
+        SeparateChainingHashST<String, Integer> hashTableSC = new SeparateChainingHashST<String, Integer>();
+
+        assert hashTableSC.get("cat") == null;
+        hashTableSC.put("cat", 1);
+        assert hashTableSC.get("cat") != null;
+        assert hashTableSC.get("cat") == 1;
+        hashTableSC.put("cat", 2);
+        assert hashTableSC.get("cat") == 2;
+        assert hashTableSC.get("dog") == null;
+        hashTableSC.put("dog", 8);
+        assert hashTableSC.get("dog") != null;
+        assert hashTableSC.get("dog") > 3;
+        assert hashTableSC.get("dog") == 8;
     }
 }
